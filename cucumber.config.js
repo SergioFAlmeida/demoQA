b79ce371a2a209d40c5b9fa85442c7feafc3012d
SergioFAlmeida <sftristam@gmail.com>
2024-11-17 18:50:02 -0300

    Delete cucumber.config.js

--- cucumber.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@badeball/cypress-cucumber-preprocessor');
-
-module.exports = defineConfig({
-  stepDefinitions: 'cypress/integration/step_definitions/**/*.js',
-  
-});
-
-
